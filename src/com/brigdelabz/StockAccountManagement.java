bece8687f862cb243f78aef0b9318a593079cb84
23Shivani <shivani.gharve.2310@gmail.com>
2021-07-27 00:01:43 +0530

    Added: Stock Management Program

---
+++ src/com/brigdelabz/StockAccountManagement.java
@@ -0,0 +1,35 @@
+package com.brigdelabz;
+
+public class StockAccountManagement {
+
+	public void addStocks(Stocks newShare) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void getStockDetails() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void calculateShareValue() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void removeStock(String shareName) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+//	public void addStocks(Stocks share);
+//	public void getStockDetails();
+//	public void calculateShareValue();
+//	public void removeStock(String name);
+//	public void sellStocks(int count, String symbol);
+//	public void buyStocks(int count, String symbol);
+//	public void addStocks(Stocks newShare) {
+
+
+
+}
